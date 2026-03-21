06f60f575a56098cff38f01d589132f604508805
[dyad] <sagarbanerjee1999@gmail.com>
2026-03-21 11:51:45 +0530

    [dyad] Fixing JSX syntax errors - wrote 1 file(s)

--- src/pages/SobrietyTracker.tsx
+++ src/pages/SobrietyTracker.tsx
@@ -796,7 +796,7 @@ const SobrietyTracker = () => {
                 </CardContent>
               </Card>
             </motion.div>
-          </>
+          </div>
         )}
       </div>
     </div>
